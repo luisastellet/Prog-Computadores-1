61c0ac344057f1ce9e27bd836aa3b72ae9e3f645
luisastellet <luisastellet@id.uff.br>
2023-04-14 09:47:48 -0300

    .

--- aula3.5.py
+++ aula3.5.py
@@ -1,4 +1,27 @@
 centavos = int(input('Quantos centavos você tem? '))
+
+umreal = centavos // 100
+centavos = centavos - (umreal * 100)
+
+cinquenta = centavos // 50
+centavos = centavos - (cinquenta * 50)
+
+vintecinco = centavos // 25
+centavos = centavos - (vintecinco * 25)
+
+dez = centavos // 10
+centavos = centavos - (dez * 10)
+
+cinco = centavos // 5
+centavos = centavos - (cinco * 5)
+
+um = centavos // 1
+centavos = centavos - (um * 1)
+
+print('Você precisará de {} moedas de 1 real, {} moedas de 50 centavos, {} moedas de 25 centavos, {} moedas de 10 centavos, {} moedas de 5 centavos e {} moedas de 1 centavo.' .format(umreal, cinquenta, vintecinco, dez, cinco, um))
+
+'''
+centavos = int(input('Quantos centavos você tem? '))
 umreal = centavos // 100
 if umreal != 0:
     centavos = centavos - (umreal * 100)
@@ -23,7 +46,18 @@ um = centavos // 1
 if um != 0:
     centavos = centavos - (um * 1)
 
-print('Você precisará de {} moedas de 1 real, {} moedas de 50 centavos, {} moedas de 25 centavos, {} moedas de 10 centavos, {} moedas de 5 centavos e {} moedas de 1 centavo.' .format(umreal, cinquenta, vintecinco, dez, cinco, um))
+print('Você precisará de {} moedas de 1 real, {} moedas de 50 centavos, {} moedas de 25 centavos, {} moedas de 10 centavos, {} moedas de 5 centavos e {} moedas de 1 centavo.' .format(umreal, cinquenta, vintecinco, dez, cinco, um)) '''
+
+
+''' OPERADORES MATEMÁTICOS: 
++ -> SOMA   
+- -> SUBTRAÇÃO   
+* -> MULTIPLICAÇÃO   
+/ -> DIVISÃO   
+// -> DIVISÃO INTEIRA(QUOCIENTE)  
+% -> RESTO DA DIVISÃO   
+** -> EXPOENCIAÇÃO '''
+
 
 
 ''' OPERADORES MATEMÁTICOS: 
